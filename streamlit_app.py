22aa6e46f5623c6c375e703fd710e55667f4e535
mtgp999 <143763209+mtgp999@users.noreply.github.com>
2023-09-01 10:06:18 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner!')
+streamlit.header('Breakfast')
+streamlit.text('Egg and Bacon roll')
+streamlit.text('Veggie salads')
+streamlit.text('Orange juice')
