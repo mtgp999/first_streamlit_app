49e6f0fa7fe143bb671d20a69c2103982c049986
mtgp999 <143763209+mtgp999@users.noreply.github.com>
2023-09-01 10:14:48 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import panda
 
-streamlit.title('My Mom/'s New Healthy Dinner!')
+streamlit.title('My Mom\'s New Healthy Dinner!')
 
 streamlit.header('🥣 Breakfast Favorites')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
