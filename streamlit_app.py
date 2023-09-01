847d3d5dc74fcf1f9d0780df0bfe33e942729f7e
mtgp999 <143763209+mtgp999@users.noreply.github.com>
2023-09-01 10:08:45 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ streamlit.title('My Parents New Healthy Dinner!')
 streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg'
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
